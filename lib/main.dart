c1d0cbc2c2df3114f3b9e1116fa4cb86083bcff3
Jimah259 <jimkip99@gmail.com>
2022-07-19 16:48:33 +0000

    Firebase Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:camera/camera.dart';
 import 'package:chatapp/Screens/CameraScreen.dart';
 import 'package:chatapp/Screens/Homescreen.dart';
 import 'package:chatapp/Screens/LoginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   cameras = await availableCameras();
 

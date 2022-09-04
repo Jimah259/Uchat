c40fdd581c194d2459eb0d9ab610aca6ec5ff88e
Frank Adu <64962641+SeibenKami@users.noreply.github.com>
2022-09-04 02:37:27 +0000

    hmm

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:chatapp/Screens/LoginScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
